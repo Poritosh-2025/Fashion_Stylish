c6b7be342535e9d29ae6ae1dd583fdf93200116e
poritosh-2025 <poritoshpal98@gmail.com>
2025-06-23 21:59:23 +0600

    update ai

---
+++ fashion_style/ai_stylist_app/__init__.py
@@ -0,0 +1,2 @@
+# AI Stylist App
+default_app_config = 'ai_stylist_app.apps.AiStylistAppConfig'
--- fashion_style/ai_stylist_app/apps.py
+++ fashion_style/ai_stylist_app/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class AiStylistAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ai_stylist_app'
+    verbose_name = 'AI Fashion Stylist'
+
+# from django.apps import AppConfig
+
+
+# class AiStylistAppConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'ai_stylist_app'
