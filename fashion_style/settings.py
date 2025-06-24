87cfd0e4469d7d7ab5563cbb9caf8ef103a7562f
Md SIamul Islam Soaib <68956777+mdsiamulislam@users.noreply.github.com>
2025-06-24 18:40:23 +0600

    Update settings.py

--- fashion_style/settings.py
+++ fashion_style/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b-&(-roid*mqa@k8ryvol8&tdg)d(p1evhiuyza(r549c4n)+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['fashion-stylish.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://fashion-stylish.onrender.com']
 PORT = os.environ.get('PORT', '8000')
 
 
